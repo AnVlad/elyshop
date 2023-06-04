faf72646548f16c208fb2c7dbce9e644f448c8a0
AnVlad <AnVlad100@gmail.com>
2023-06-04 12:43:26 +0300

    app with using mongoose

---
+++ src/server/controllers/menuList.js
@@ -0,0 +1,25 @@
+const menuListRouter = require('express').Router();
+const Menu = require('../modules/menu');
+
+menuListRouter.get('/', async (request, response) => {
+  const menuList = await Menu.find({});
+  response.json(menuList);
+});
+
+menuListRouter.post('/', async (request, response) => {
+  const body = request.body;
+
+  const newItem = new Menu({
+    name: body.name,
+    image: body.image,
+    price: body.price,
+    restaurant: body.restaurant,
+    popularity: body.popularity,
+  });
+
+  const savedItem = await newItem.save();
+
+  response.status(201).json(savedItem);
+});
+
+module.exports = menuListRouter;
--- src/server/server.js
+++ src/server/server.js
@@ -1,37 +1,27 @@
-menuList = require('../menuList');
+require('dotenv').config();
 
-const fs = require('fs');
+const mongoUrl = process.env.MONGODB_URI;
+const PORT = 3001;
 
+const mongoose = require('mongoose');
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+
+const menuListRouter = require('./controllers/menuList');
+
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use(morgan(':method :url :status :res[content-length] :response-time :data'));
+app.use('/menuList', menuListRouter);
+
+mongoose.connect(mongoUrl);
 
 morgan.token('data', (req, res) => {
   return JSON.stringify(req.body);
 });
-
-// let's pretend it's a database
-const saveData = (data) => {
-  const filePath = 'src/server/data.txt';
-  fs.writeFileSync(filePath, JSON.stringify(data));
-};
-//
-
-app.get('/elyshop', (request, response) => {
-  response.json(menuList);
-});
-
-app.post('/elyshop', (request, response) => {
-  saveData(request.body);
-  response.status(202).end();
-});
-
-const PORT = 3001;
 
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
